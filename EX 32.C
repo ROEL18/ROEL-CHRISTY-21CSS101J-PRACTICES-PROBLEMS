fcc5a8b8ee13cb248de7faa305e3f2568f7487bc
ROEL18 <113873579+ROEL18@users.noreply.github.com>
2022-11-22 11:06:35 +0530

    Create EX 32.C

---
+++ EX 32.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+  int a,b,c;
+  printf("numerator:");
+  scanf("%d",&a);
+  printf("denominator");
+  scanf("%d",&b);
+  c=a/b;
+  printf("result of division:%d",c);
