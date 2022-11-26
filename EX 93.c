2c5860b3ce17520b082b1a821400c8fa1ede15c9
ROEL18 <113873579+ROEL18@users.noreply.github.com>
2022-11-26 08:05:46 +0530

    Create EX 93.c

---
+++ EX 93.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{ printf("RA2211042010019\n");
+  float length,breadth;
+  printf("enter length,&breadth");
+  scanf("%f",&length,&breadth);
+  float area=length*breadth;
+  printf("The area=%f",area);
+}
