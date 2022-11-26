3063fcb8a474d9a3e9b965a044b3ea7688da3c12
ROEL18 <113873579+ROEL18@users.noreply.github.com>
2022-11-26 08:08:09 +0530

    Create EX 96.c

---
+++ EX 96.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  
+  
+  float side, area;
+
+ 
+  printf("Enter Side");
+  scanf("%f",&side);
+
+ 
+ 
+  area =side * side ;
+  printf("Area of square is: %f",area);
+
+  return 0;
+}
