c9ccff2d319fde94660dab954d41639d3281f7aa
ROEL18 <113873579+ROEL18@users.noreply.github.com>
2022-11-26 08:10:41 +0530

    Create EX 100.c

---
+++ EX 100.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main()
+{
+  
+ 
+    float fahrenheit, celsius;
+    printf("Enter Temperature Value (in Celsius):\n");
+    scanf("%f", &celsius);
+    fahrenheit=(celsius*1.8)+32;
+    printf(" fahrenheit= %f", fahrenheit);
+    return 0;
+}
